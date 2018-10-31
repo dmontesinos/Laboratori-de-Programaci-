ae42c61632c117c74ecaf5e1f4a3edb4cb8623f5
Daniel <danimontesinos1@gmail.com>
2018-10-31 15:40:12 +0100

    Tema 4 - Recursividad Ejercicio 3 empezado. NO funciona.

--- Tema 4/Exercici 3/Exercici 3/Exercici3.cpp
+++ Tema 4/Exercici 3/Exercici 3/Exercici3.cpp
@@ -1,7 +1,31 @@
 #include <list>
+#include <iostream>
+
 using namespace std;
+
+
+int MaxElsRec(list<int> l, list<int>::iterator it)
+{
+	int max = 0;
+
+	if (it != l.end())
+	{
+		if (*it > max)
+		{
+			max = *it;
+			MaxElsRec(l, ++it);
+		}
+		return max;
+	}
+	else {
+		return max;
+	}
+}
 
 int MaxElementsLlista(list<int> &l)
 {
-
+	cout << "El maximo de la lista es: " << endl;
+	int maximo = MaxElsRec(l, l.begin());
+	return maximo;
+	system("pause");
 }
